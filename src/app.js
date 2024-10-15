17280c069d0e5505777f6919d803b54ec2dc1ed4
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2024-10-15 23:06:23 +0530

    HTTP request :GET and POST :wrench:

--- src/app.js
+++ src/app.js
@@ -2,16 +2,12 @@ const express = require("express")
 const app = express();
 const port = 3000;
 
-app.use("/", (req, res) => {
-    res.send("Main route")
+app.get("/user", (req, res) => {
+    res.send("Get Request ")
 })
 
-app.use("/test", (req, res) => {
-    res.send("Server started ")
-})
-
-app.use("/main", (req, res) => {
-    res.send("another route")
+app.post("/user", (req, res) => {
+    res.send("POST request ")
 })
 
 app.listen(port, () => {
