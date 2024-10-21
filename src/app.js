dbfe16ff3d0e96077bb175cdaf6e1d5d3f6d60de
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2024-10-21 19:22:32 +0530

    Multiple Route Handlers and next parameter :heavy_plus_sign:

--- src/app.js
+++ src/app.js
@@ -2,38 +2,29 @@ const express = require("express")
 const app = express();
 const port = 3000;
 
-app.get("/user", (req, res) => {
-    res.send("GET Request ")
-})
+app.use(
+    "/user",
+    
+    [(req, res, next) => {
+        console.log("1st route Handler")
+        next()
+    },
+    (req, res, next) => {
+        console.log("2nd route Handler")
+        // res.send("Second Response")
+        next()
+    },
+    (req, res, next) => {
+        console.log("3rd route Handler")
+        // res.send("Third Response")
+        next()
+    },
+    (req, res, next) => {
+        console.log("4th route Handler")
+        res.send("Fourth Response")
 
-app.post("/user", (req, res) => {
-    res.send("POST Request ")
-})
-
-app.put("/user", (req, res) => {
-    res.send("PUT Request")
-})
-
-app.delete("/user", (req, res) => {
-    res.send("Delete Request")
-})
-
-app.patch("/user", (req, res) => {
-    res.send("PATCH Request")
-})
-
-//advance routing
-app.get("/user?d", (req, res) => {
-    res.send("PATCH Request")
-})
-
-app.get("/user+c", (req, res) => {
-    res.send({firstName:"Akshad", Surname:"Jaiswal"})
-})
-
-app.get("/user*cd", (req, res) => {
-    res.send({firstName:"Akshad*", Surname:"Jaiswal*"})
-})
+    }]
+)
 
 app.listen(port, () => {
     console.log("Server started running on port " + port)
