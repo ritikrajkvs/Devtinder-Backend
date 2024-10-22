122d4fce3e9802dc85366e4236b50c502f1a127b
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2024-10-22 18:30:44 +0530

    :Error Handling Middleware ✅

--- src/app.js
+++ src/app.js
@@ -21,6 +21,19 @@ app.get("/user/login", (req, res) => {
     res.send("User Logged in successfully (No auth required)")
 })
 
+app.get("/errorHandler", (req, res) => {
+    throw new Error("Random Error you have to solve it");
+    res.send("Error handling");
+})
+
+//error handling middleware
+app.use("/", (err, req, res, next) => {
+    if (err) {
+        // console.log(err)
+        res.status(500).send("Soemthing went wrong")
+    }
+})
+
 app.listen(port, () => {
     console.log("Server started running on port " + port)
 })
