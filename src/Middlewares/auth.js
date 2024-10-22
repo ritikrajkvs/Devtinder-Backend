552e420f55f4f878c421efa13a3027a8bc6b1ea4
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2024-10-22 18:13:02 +0530

    Middlewares and auth middlewares ✅

---
+++ src/Middlewares/auth.js
@@ -0,0 +1,27 @@
+const adminAuth = (req, res, next) => {
+    const token = "999";
+    const isAuthorizedAdmin = token === "999";
+    console.log("Admin Authentication checking")
+    if (!isAuthorizedAdmin) {
+        res.status(401).send("Unauthorized Admin")
+    } else {
+        next();
+    }
+
+}
+
+const userAuth = (req, res, next) => {
+    const token = "777";
+    const isAuthorizedAdmin = token === "777";
+    console.log("User Authentication checking")
+    if (!isAuthorizedAdmin) {
+        res.status(401).send("Unauthorized Admin")
+    } else {
+        next();
+    }
+
+}
+module.exports = {
+    adminAuth,
+    userAuth
+}
--- src/app.js
+++ src/app.js
@@ -1,30 +1,25 @@
 const express = require("express")
 const app = express();
 const port = 3000;
+const { adminAuth, userAuth } = require("./Middlewares/auth")
 
-app.use(
-    "/user",
-    
-    [(req, res, next) => {
-        console.log("1st route Handler")
-        next()
-    },
-    (req, res, next) => {
-        console.log("2nd route Handler")
-        // res.send("Second Response")
-        next()
-    },
-    (req, res, next) => {
-        console.log("3rd route Handler")
-        // res.send("Third Response")
-        next()
-    },
-    (req, res, next) => {
-        console.log("4th route Handler")
-        res.send("Fourth Response")
+//Handle user authentication for all admin routes using middlewares
+app.use("/admin", adminAuth)
+app.get("/admin/getAllData", (req, res) => {
+    res.send("All data Generated")
+})
 
-    }]
-)
+app.get("/admin/deleteData", (req, res) => {
+    res.send("Data Deleted")
+})
+
+app.get("/user", userAuth, (req, res) => {
+    res.send("User Data Sent")
+})
+
+app.get("/user/login", (req, res) => {
+    res.send("User Logged in successfully (No auth required)")
+})
 
 app.listen(port, () => {
     console.log("Server started running on port " + port)
