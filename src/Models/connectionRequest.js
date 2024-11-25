6589d359862bce32a48c12431434b01930da7d58
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2024-11-25 21:35:28 +0530

    New model Schema for Connection Request :heavy_plus_sign:

---
+++ src/Models/connectionRequest.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const connectionRequest = new mongoose.Schema({
+
+    fromUserId: {
+        type: mongoose.Schema.Types.ObjectId,
+        requ
+    },
+    toUserId: {
+        type: mongoose.Schema.Types.ObjectId
+    },
+    status: {
+        type: String,
+        enum: {
+            values: ["ignore", "accepted", "rejected", "intrested"],
+            message: `{VALUES} is incorrect status type`
+        }
+    }
+},
+    {
+        timestamps: true
+    }
+);
