b992eb3b20f01225a8d8bb4bf03c5c62edbdf296
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2025-01-13 19:49:11 +0530

    API: All connections API✅

--- src/routes/user.js
+++ src/routes/user.js
@@ -3,6 +3,8 @@ const userRouter = express.Router();
 const { userAuth } = require("../Middlewares/auth");
 const { ConnectionRequestModel } = require("../Models/connectionRequest");
 
+const USER_SAFE_DATA = "firstName lastName photoURL about age gender";
+
 //Get all the pending connection request for the logged in user
 userRouter.get("/user/requests/recieved", userAuth, async (req, res) => {
   try {
@@ -10,7 +12,7 @@ userRouter.get("/user/requests/recieved", userAuth, async (req, res) => {
     const connectionRequests = await ConnectionRequestModel.find({
       toUserId: loggedInUser._id,
       status: "intrested",
-    }).populate("fromUserId", "firstName lastName photoURL about age gender");
+    }).populate("fromUserId", USER_SAFE_DATA);
     if (connectionRequests) {
       return res.status(200).json({
         connectionRequests,
@@ -21,4 +23,29 @@ userRouter.get("/user/requests/recieved", userAuth, async (req, res) => {
   }
 });
 
+userRouter.get("/user/connections", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+
+    const connectionRequests = await ConnectionRequestModel.find({
+      $or: [{ toUserId: loggedInUser._id }, { fromUserId: loggedInUser._id }],
+      status: "accepted",
+    }).populate("fromUserId", USER_SAFE_DATA);
+
+    if (!connectionRequests) {
+      return res.status(404).json({
+        message: "No connection found",
+        success: "false",
+      });
+    }
+
+    const data = connectionRequests.map((row) => row.fromUserId);
+
+    res.status(200).json({
+      data,
+    });
+  } catch (error) {
+    res.status(400).send("ERROR :" + error.message);
+  }
+});
 module.exports = userRouter;
