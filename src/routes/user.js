cb1c408bb3b0cf9c38c586f85de7c0fc5c56ca88
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2025-01-14 19:28:05 +0530

    API: Connections API ✅

--- src/routes/user.js
+++ src/routes/user.js
@@ -28,18 +28,20 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
     const loggedInUser = req.user;
 
     const connectionRequests = await ConnectionRequestModel.find({
-      $or: [{ toUserId: loggedInUser._id }, { fromUserId: loggedInUser._id }],
-      status: "accepted",
-    }).populate("fromUserId", USER_SAFE_DATA);
+      $or: [
+        { toUserId: loggedInUser._id, status: "accepted" },
+        { fromUserId: loggedInUser._id, status: "accepted" },
+      ],
+    }).populate("fromUserId toUserId", USER_SAFE_DATA);
 
-    if (!connectionRequests) {
-      return res.status(404).json({
-        message: "No connection found",
-        success: "false",
-      });
-    }
+    // console.log(connectionRequests);
 
-    const data = connectionRequests.map((row) => row.fromUserId);
+    const data = connectionRequests.map((row) => {
+      if (row.fromUserId._id.toString() == loggedInUser._id.toString()) {
+        return row.toUserId;
+      }
+      return row.fromUserId;
+    });
 
     res.status(200).json({
       data,
