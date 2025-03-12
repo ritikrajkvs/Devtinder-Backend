ca34f45b4498648c05ca0e51959095cc91ee193e
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2025-03-12 22:24:05 +0530

    Response message update

--- src/Middlewares/auth.js
+++ src/Middlewares/auth.js
@@ -4,7 +4,7 @@ const userAuth = async (req, res, next) => {
   try {
     const { token } = req.cookies;
     if (!token) {
-      return res.status(401).send("Please Login");
+      return res.status(401).send("Please Login or Signup");
     }
 
     const deocodedObj = await jwt.verify(token, process.env.JWT_SECRET);
